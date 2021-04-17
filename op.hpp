8187f04caa923c1dce700919357444fbdb38ebb6
rohanbehera03 <65427904+rohanbehera03@users.noreply.github.com>
2021-04-17 14:00:40 -0700

    Update op.hpp

--- op.hpp
+++ op.hpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-<<<<<<< HEAD
 
 class Op : public Base {
     private:
@@ -22,20 +21,4 @@ class Op : public Base {
 };
 
 #endif //__OP_HPP__
-=======
-   
-class Op : public Base {
-    private:
-          double num;
-    public:
-         Op(double value) : Base() { num = value; }
-         virtual double evaluate() { return num; } //return value of a node
-         virtual std::string stringify() {
-                 std::stringstream out;
-                 out << num;
-                 return out.str();
-          } //return a string representation of node 
-  };
-  
- #endif //__OP_HPP__
->>>>>>> e78529d3e11530af27b1f2f8b6be0c12b6427add
+
