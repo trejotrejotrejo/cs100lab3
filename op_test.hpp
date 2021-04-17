2119bc1a5a5d2f130113398c3ee4b86e100e283e
rohanbehera03 <rohanbehera03@gmail.com>
2021-04-16 17:50:06 -0700

    Add op.hpp and op_test.hpp

--- op_test.hpp
+++ op_test.hpp
@@ -34,47 +34,5 @@ TEST(OpTest, OpEvaluateStringify) {
     EXPECT_EQ(test->evaluate(), 28990);
 }
 
-//Unit tests for add function
-TEST(AddingTest, AddEvaluateZero) {
-    Op* test1 = new Op(0);
-    Op* test2 = new Op(0);
-    Base* ad = new Add(test1, test2);
-    EXPECT_EQ(ad->evaluate(), 0);
-}
-
-TEST(AddingTest, AddEvaluatePositiveVal) {
-     Op* test1 = new Op(46);
-     Op* test2 = new Op(92);
-     Base* ad = new Add(test1, test2);
-     EXPECT_EQ(ad->evaluate(), 138);
- }
-
-TEST(AddingTest, AddEvaluateNegativeVal) {
-    Op* test1 = new Op(4);
-    Op* test2 = new Op(-11);
-    Base* ad = new Add(test1, test2);
-    EXPECT_EQ(ad->evaluate(), -7);
-}
-
-TEST(AddingTest, AddEvaluateFloatingPt) {
-     Op* test1 = new Op(7.19);
-     Op* test2 = new Op(17.38);
-     Base* ad = new Add(test1, test2);
-     EXPECT_EQ(ad->evaluate(), 24.57);
-}
-
-TEST(AddingTest, AddEvaluateLargeNum) {
-    Op* test1 = new Op(6180010);
-    Op* test2 = new Op(4939823);
-    Base* ad = new Add(test1, test2);
-    EXPECT_EQ(ad->evaluate(), 11119833);
-}
-
-TEST(AddingTest, AddEvaluateStringify) {
-    Op* test1 = new Op(7);
-    Op* test2 = new Op(14);
-    Base* ad = new Add(test1, test2);
-    EXPECT_EQ(ad->stringify(), "7 + 14");
-}
-
 #endif //__OP_TEST_HPP__
+
