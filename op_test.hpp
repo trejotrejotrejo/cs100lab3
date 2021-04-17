717bb1eacc0e13a0b326572039430d53bb7383bb
rohanbehera03 <rohanbehera03@gmail.com>
2021-04-16 20:36:24 -0700

    Adding pow.hpp and update op_test.hpp files

--- op_test.hpp
+++ op_test.hpp
@@ -2,12 +2,44 @@
 #define __OP_TEST_HPP__
 
 #include "gtest/gtest.h"
-
+#include "base.hpp"
+#include "pow.hpp"
 #include "op.hpp"
 
-TEST(OpTest, OpEvaluateNonZero) {
-    Op* test = new Op(8);
-    EXPECT_EQ(test->evaluate(), 8);
+
+   TEST(PowerTest, PowEvaluateZero) {
+    Op* test1 = new Op(14);
+    Op* test2 = new Op(0);
+    Base* power = new Pow(test1, test2);
+    EXPECT_EQ(power->evaluate(), 1);
+}
+
+TEST(PowerTest, PowEvaluateNeg) {
+    Op* test1 = new Op(-9);
+    Op* test2 = new Op(3);
+    Base* power = new Pow(test1, test2);
+    EXPECT_EQ(power->evaluate(), -729);
+}
+
+ TEST(PowerTest, PowEvaluatePos) {
+     Op* test1 = new Op(4);
+     Op* test2 = new Op(3);
+     Base* power = new Pow(test1, test2);
+     EXPECT_EQ(power->evaluate(), 64);
+ }
+ 
+ TEST(PowerTest, PowFloatingPt) {
+      Op* test1 = new Op(12.7);
+      Op* test2 = new Op(5.4);
+      Base* power = new Pow(test1, test2);
+      EXPECT_FLOAT_EQ(power->evaluate(), 913145.7311);
+ }
+ 
+ TEST(PowerTest, PowStringify) {
+      Op* test1 = new Op(20);
+      Op* test2 = new Op(5);
+      Base* power = new Pow(test1, test2);
+      EXPECT_EQ(power->stringify(), "20 ^ 5");
 }
 
 #endif //__OP_TEST_HPP__
