d9fdfd346bd648a5437d67fa7b37d749f59fa7d4
rohanbehera03 <rohanbehera03@gmail.com>
2021-04-17 13:54:52 -0700

    Fix merge conflict

--- op_test.hpp
+++ op_test.hpp
@@ -1,6 +1,14 @@
 #ifndef __OP_TEST_HPP__
-#define __OP_TEST_HPP__
+ #define __OP_TEST_HPP__
+  
+  #include "gtest/gtest.h"
+  #include "base.hpp"
+  #include "op.hpp"
+  #include "add.hpp"
+  #include "sub.hpp"
+  #include "pow.hpp"
 
+<<<<<<< Updated upstream
 #include "gtest/gtest.h"
 #include "gtest/gtest.h"
 #include "base.hpp"
@@ -83,12 +91,161 @@ TEST(DivTest, DivEvaluatePositiveVal) {
 #include "mult.hpp"
 #include "div.hpp"
 #include "rand.hpp"
+=======
+TEST(OpTest, OpEvaluateZero) {
+      Op* test = new Op(0);
+      EXPECT_EQ(test->evaluate(), 0);
+ }
+  
+ TEST(OpTest, OpEvaluateFloatingPt) {
+      Op* test = new Op(3.5);
+      EXPECT_EQ(test->evaluate(), 3.5);
+ }
+  
+  TEST(OpTest, OpEvaluateNeg) {
+      Op* test = new Op(-24);
+      EXPECT_EQ(test->evaluate(), -24);
+  }
+  
+ TEST(OpTest, OpEvaluateLargeNum) {
+     Op* test = new Op(68570450);
+      EXPECT_EQ(test->evaluate(), 68570450);
+  }
+  
+  TEST(OpTest, OpEvaluateStringify) {
+      Op* test = new Op(28990);
+      EXPECT_EQ(test->evaluate(), 28990);
+  }
 
-TEST(OpTest, OpEvaluateNonZero) {
-    Op* test = new Op(8);
-    EXPECT_EQ(test->evaluate(), 8);
+ TEST(AddingTest, AddEvaluateZero) {
+      Op* test1 = new Op(0);
+      Op* test2 = new Op(0);
+      Base* ad = new Add(test1, test2);
+    EXPECT_EQ(ad->evaluate(), 0);
+ }
+  
+  TEST(AddingTest, AddEvaluatePositiveVal) {
+       Op* test1 = new Op(46);
+       Op* test2 = new Op(92);
+       Base* ad = new Add(test1, test2);
+       EXPECT_EQ(ad->evaluate(), 138);
+   }
+  
+  TEST(AddingTest, AddEvaluateNegativeVal) {
+      Op* test1 = new Op(4);
+      Op* test2 = new Op(-11);
+      Base* ad = new Add(test1, test2);
+      EXPECT_EQ(ad->evaluate(), -7);
+ }
+ 
+ TEST(AddingTest, AddEvaluateFloatingPt) {
+      Op* test1 = new Op(7.19);
+      Op* test2 = new Op(17.38);
+      Base* ad = new Add(test1, test2);
+      EXPECT_EQ(ad->evaluate(), 24.57);
+ }
+ 
+ TEST(AddingTest, AddEvaluateLargeNum) {
+     Op* test1 = new Op(6180010);
+     Op* test2 = new Op(4939823);
+     Base* ad = new Add(test1, test2);
+      EXPECT_EQ(ad->evaluate(), 11119833);
+ }
+ 
+ TEST(AddingTest, AddEvaluateStringify) {
+     Op* test1 = new Op(7);
+     Op* test2 = new Op(14);
+     Base* ad = new Add(test1, test2);
+     EXPECT_EQ(ad->stringify(), "7 + 14");
 }
 
+TEST(SubtractTest, SubEvaluatePositive) {
+      Op* test1 = new Op(9);
+      Op* test2 = new Op(4);
+      Base* subtraction = new Sub(test1, test2);
+      EXPECT_EQ(subtraction->evaluate(), 5);
+  }
+  
+ TEST(SubtractTest, SubEvaluateZero) {
+       Op* test1 = new Op(0);
+       Op* test2 = new Op(0);
+       Base* subtraction = new Sub(test1, test2);
+       EXPECT_EQ(subtraction->evaluate(), 0);
+  }
+  
+ TEST(SubtractTest, SubEvaluateNegative) {
+      Op* test1 = new Op(-5);
+      Op* test2 = new Op(-3);
+      Base* subtraction = new Sub(test1, test2);
+      EXPECT_EQ(subtraction->evaluate(), -2);
+ }
+ 
+TEST(SubtractTest, SubLargeNumber) {
+     Op* test1 = new Op(2006727);
+     Op* test2 = new Op(8060612);
+     Base* subtraction = new Sub(test1, test2);
+    EXPECT_EQ(subtraction->evaluate(), -6053885);
+ }
+ 
+ TEST(SubtractTest, SubEvaluateFloatingPt) {
+      Op* test1 = new Op(31.91);
+      Op* test2 = new Op(5.45);
+      Base* subtraction = new Sub(test1, test2);
+      EXPECT_EQ(subtraction->evaluate(), 26.46);
+  }
+ 
+ TEST(SubtractTest, SubEvaluateStringify) {
+     Op* test1 = new Op(44);
+     Op* test2 = new Op(56);
+     Base* subtraction = new Sub(test1, test2);
+     EXPECT_EQ(subtraction->stringify(), "44 - 56");
+ }
+
+  TEST(PowerTest, PowEvaluateZero) {
+    Op* test1 = new Op(14);
+    Op* test2 = new Op(0);
+    Base* power = new Pow(test1, test2);
+    EXPECT_EQ(power->evaluate(), 1);
+}
+
+TEST(PowerTest, PowEvaluateNeg) {
+    Op* test1 = new Op(-9);
+    Op* test2 = new Op(3);
+    Base* power = new Pow(test1, test2);
+    EXPECT_EQ(power->evaluate(), -729);
+}
+
+ TEST(PowerTest, PowEvaluatePos) {
+     Op* test1 = new Op(4);
+     Op* test2 = new Op(3);
+     Base* power = new Pow(test1, test2);
+     EXPECT_EQ(power->evaluate(), 64);
+ }
+ 
+ TEST(PowerTest, PowFloatingPt) {
+      Op* test1 = new Op(12.7);
+      Op* test2 = new Op(5.4);
+      Base* power = new Pow(test1, test2);
+      EXPECT_FLOAT_EQ(power->evaluate(), 913145.7311);
+ }
+ 
+ TEST(PowerTest, PowStringify) {
+      Op* test1 = new Op(20);
+      Op* test2 = new Op(5);
+      Base* power = new Pow(test1, test2);
+      EXPECT_EQ(power->stringify(), "20 ^ 5");
+}
+>>>>>>> Stashed changes
+
+//unit tests for division function
+TEST(DivTest, DivEvaluateZero) {
+    Op* test1 = new Op(0);
+    Op* test2 = new Op(5);
+    Base* divv = new Div(test1, test2);
+    EXPECT_EQ(divv->evaluate(), 0);
+}
+
+<<<<<<< Updated upstream
 TEST(OpTest, OpEvaluateZero) {
     Op* test = new Op(0);
     EXPECT_EQ(test->evaluate(), 0);
@@ -148,6 +305,8 @@ TEST(DivTest, DivEvaluateZero) {
     EXPECT_EQ(divv->evaluate(), 0);
 }
 
+=======
+>>>>>>> Stashed changes
 TEST(DivTest, DivEvaluatePositiveVal) {
     Op* test1 = new Op(49);
     Op* test2 = new Op(7);
@@ -176,4 +335,9 @@ TEST(DivTest, DivEvaluateStringify) {
     EXPECT_EQ(divv->stringify(), "55 / 11");
 } 
 
+<<<<<<< Updated upstream
 #endif //__OP_TEST_HPP__
+=======
+ #endif //__OP_TEST_HPP__
+
+>>>>>>> Stashed changes
