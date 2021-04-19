0515829d3fad731216e260df4fba8863e6b61ce6
trejotrejotrejo <alextrejo10101@gmail.com>
2021-04-18 21:02:05 -0700

    Fix merge

--- op.hpp
+++ op.hpp
@@ -4,10 +4,40 @@
 #include "base.hpp"
 
 class Op : public Base {
+    private: 
+	double num;
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
+        Op(double value) : Base() { num = value; }
+        virtual double evaluate() { return num; }
         virtual std::string stringify() { return ""; }
 };
+
+class SevenOpMock : public Base (
+    private:
+	double num;
+    public:
+	SevenOpMock(double value) : Base() { num = value; }
+	virtual double evaluate() { return 7.5; }
+	virtual string stringify() { return "7.5"; )
+}
+
+class NegativeOpMock : public Base (
+    private:
+	double num;
+    public:
+        NegativeOpMock(double value) : Base() { num = value; } 
+        virtual double evaluate() { return -7.5; }
+        virtual string stringify() { return "-7.5"; )
+}
+
+class ZeroOpMock : public Base (
+    private:
+	double num;
+    public:
+        ZeroOpMock(double value) : Base() { num = value; }
+        virtual double evaluate() { return 0; }
+        virtual string stringify() { return "0"; )
+}
+
 
 #endif //__OP_HPP__
