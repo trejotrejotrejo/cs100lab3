a98b7510d8d5275270d889ade670859bbcc949c0
trejotrejotrejo <alextrejo10101@gmail.com>
2021-04-18 20:48:28 -0700

    Rand Function

--- op_test.hpp
+++ op_test.hpp
@@ -4,10 +4,20 @@
 #include "gtest/gtest.h"
 
 #include "op.hpp"
+#include "rand.hpp"
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
     EXPECT_EQ(test->evaluate(), 8);
+}
+
+TEST(RandTest, RandEvaluate) {
+    Rand* test = new Rand();
+    EXPECT_EQ(test->evaluate(), 100);
+
+TEST(RandTest, RandEvaluateStringify) {
+    Rand* test = new Rand();
+    EXPECT_EQ(test->evaluate(), stod(test->stringify()), 0.001);
 }
 
 #endif //__OP_TEST_HPP__
